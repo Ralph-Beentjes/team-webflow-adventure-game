e540bf355810f7c80b5046064025b131b3415f61
Noa Tzuberi <noatzu3@gmail.com>
2026-09-10 13:41:57 +0300

    add second game challenge

--- JavaScript/adventureGame.js
+++ JavaScript/adventureGame.js
@@ -11,6 +11,9 @@ let usedAI = false;
 let userChoiceSecondChallenge = '';
 
 function firstChallenge(){
+    liedOnResume = false;
+    usedAI = false;
+    
     let userInput = prompt('Fred needs to write the best possible resume to get the job. What should he do?\n\n' +
         '1. Write a resume that is honest and talks about his lackluster skills and experiences, but with a certain charm and humor\n' +
         '2. Write a resume that is full of exaggerations and not totally true, but makes him look like a perfect candidate\n' +
@@ -23,7 +26,7 @@ function firstChallenge(){
     }
 
     if (userInput === null){
-        console.log('Fred is too scared to make a decision and does nothing. This way he will never get a job. He can try again however, by typing game() and press enter.')
+        console.log('Fred is too scared to make a decision and does nothing. This way he will never get a job. He can try again however, by typing play() and press enter.');
         return;
     }
 
@@ -33,7 +36,7 @@ function firstChallenge(){
             secondChallenge();
             break
         case '2':
-            console.log('Fred decided to exaggerate his skills. Naughty! But it does make him see like the perfect candidate.')
+            console.log('Fred decided to exaggerate his skills. Naughty! But it does make him seem like the perfect candidate.')
             liedOnResume = true;
             secondChallenge();
             break
@@ -50,6 +53,8 @@ function firstChallenge(){
 }
 
 function secondChallenge() {
+    userChoiceSecondChallenge = '';
+
     let userInput = prompt('Fred has been invited to a call with a recruiter. During the call, the recruiter asks him about his skills and previous experiences. What should Fred do?\n\n' +
         '1. Be honest and talk about how he doesn\'t have much experience, but that he is eager to learn\n' +
         '2. Talk about what a great asset to the company he would be, without going into any details\n' +
@@ -62,27 +67,46 @@ function secondChallenge() {
     }
 
     if (userInput === null){
-        console.log('Fred has a blackout and says absolutely nothing. This way he will never get a job. He can try again however, by typing game() and press enter.')
+        console.log('Fred has a blackout and says absolutely nothing. That won\'t help him get a job whatsoever. He can try again however, by typing play() and press enter.');
         return;
     }
 
     switch (userInput) {
         case '1':
-            console.log('')
+            console.log('Fred decided to be honest. He is a great asset to the company and will be a great fit for the team.')
             userChoiceSecondChallenge = '1';
         break;
         case '2':
-            console.log('')
+            console.log('Fred decided to talk about how great he would be for the company. He is a great asset to the company and will be a great fit for the team.')
             userChoiceSecondChallenge = '2';
         break;
         case '3':
-            console.log('')
+            console.log('Fred decided to lie about his experiences and skills. He is a great asset to the company and will be a great fit for the team.')
             userChoiceSecondChallenge = '3';
         break;
     }
+
+    if (liedOnResume == false && userChoiceSecondChallenge == '1') {
+        console.log('Fred has been honest all this way and it has paid off. He has been invited to the next round of interviews at the company headquarters.')
+        waitingRoom();
+    } else if (liedOnResume && userChoiceSecondChallenge == '3') {
+        console.log('Fred hasn\'t been honest, but bluffing has worked until now. He has been invited to the next round of interviews at the company headquarters.')
+        waitingRoom();
+    } else if (usedAI && userChoiceSecondChallenge == '2') {
+        console.log('Fred still has no idea what he has "done" in the past, but somehow he is through. He has been invited to the next round of interviews at the company headquarters.')
+        waitingRoom();
+    } else {
+        return "The recruiter saw through Fred's bullshit and he has been rejected. Back to the job market he goes. He can try again  by typing play() and press enter."
+    }
+
 }
 
+function waitingRoom() {
+    let userInput = prompt('Fred has arrived at the company headquarters and is waiting for his interview in the waiting room. He is q')
+    
+}
 
 function play() {
-
+    firstChallenge();
 }
+
