d2639e2eb0280712060ed74d65e1941c96cff481
Noa Tzuberi <noatzu3@gmail.com>
2026-09-10 12:34:46 +0300

    update first console message

--- JavaScript/adventureGame.js
+++ JavaScript/adventureGame.js
@@ -1,4 +1,13 @@
-alert('👾 WELCOME TO THE ADVENTURE OF YOUR LIFE 👾\n\n' +
+alert('💸 WELCOME TO THE JOB MARKET 🎮\n\n' +
     'Click OK and open the console to play an adventure game \n\n' + 
     'Windows/Linux: Ctrl + Shift + J\n' +
     'Mac: Cmd + Option + J')
+
+console.log('Fred is just done with his college and is looking for a job in the IT-sector. After trying again and again, he is still unemployed, but now he has spotted maybe his dream job.\n' +
+    'It\'s your job to make sure Fred gets the job. But it will certainly not be easy, as there are many obstacles in his way. If you are sure you can help him, type in play() and press enter')
+
+
+
+    function play() {
+
+}
