2d6995d684dc70b57d65aa20669ca42eb2b3ed3b
Ralph Beentjes <ralphsimonbeentjes@gmail.com>
2026-09-10 11:06:00 +0200

    Added an alert for console instructions

---
+++ JavaScript/adventureGame.js
@@ -0,0 +1,4 @@
+alert('👾 WELCOME TO THE ADVENTURE OF YOUR LIFE 👾\n\n' +
+    'Click OK and open the console to play an adventure game \n\n' + 
+    'Windows/Linux: Ctrl + Shift + J\n' +
+    'Mac: Cmd + Option + J')
